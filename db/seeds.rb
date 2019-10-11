d45f2f98b7374fe5eb89b5cb35e7f51e9d7a997a
aparkening <aaron.parkening@gmail.com>
2019-10-11 15:53:45 -0400

    Add seed scenario for combined_ingredient object.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,15 +3,34 @@
 
 
 # Make User, recipe, ingredient
-user = User.create(name:"Steve Tester", password:"testing", role:"User", organization:"A&E Bakery")
+user1 = User.create(name:"Steve Tester", password:"testing", role:"User", organization:"A&E Bakery")
 
-user_r = user.recipes.create(name:"Apple Pie", servings:8) 
+user1_r1 = user1.recipes.create(name:"Apple Pie", servings:8) 
 
-ing = Ingredient.create(name:"flour", cost:12.50, cost_size:5, cost_unit:"pounds")
+ing1 = Ingredient.create(name:"flour", cost:12.50, cost_size:5, cost_unit:"pounds")
 
 # Add ingredient to recipe
-user_r.recipe_ingredients.build(ingredient:ing, ingredient_amount:1.5, ingredient_unit:"cups")
+r1_ing1 = user1_r1.recipe_ingredients.build(ingredient:ing1, ingredient_amount:1.5, ingredient_unit:"cups")
   # get name with user_r.recipe_ingredients.first.ingredient.name
 
 # Make ingredient cost specific to user.
-user_ing = user.user_ingredient_costs.build(ingredient:ing, cost:14.00, cost_size:1, cost_unit:"pounds")
+user1_ing1 = user1.user_ingredient_costs.build(ingredient:ing1, cost:14.00, cost_size:1, cost_unit:"pounds")
+
+
+### Future:
+# Add ingredient to recipe through CombinedIngredient object, instead of Ingredient and UserIngredient
+# Want to access:
+# ingredient cost, cost size, cost unit, and name
+# recipe_ingredient amount and unit
+# weight_volume_conversion weight size, weight unit, volume size, and volume unit
+# Calculate: ingredient cost in this recipe
+combo_ing1 = CombinedIngredient.new(r1_ing1)
+
+
+### Test
+# - User add flag to ingredient
+# - User add category
+# - User add recipe to category
+# - Add weight volume conversion to weight_volume_conversion table
+# - Find all recipes (from user) that ingredient is used in
+# - Calculate recipe cost using weight_volume_conversion table
