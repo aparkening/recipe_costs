9ffaba6dfbdad65658a111713dd16d6751b1da96
aparkening <aaron.parkening@gmail.com>
2019-10-14 11:10:00 -0400

    Add more cost_unit logic to combined_ingredient.

--- app/models/combined_ingredient.rb
+++ app/models/combined_ingredient.rb
@@ -4,9 +4,9 @@ class CombinedIngredient
 
   # Add ingredient to recipe through CombinedIngredient object, instead of Ingredient and UserIngredient
   # Want to access:
-  # ingredient cost, cost size, cost unit, and name
-  # recipe_ingredient amount and unit
-  # weight_volume_conversion weight size, weight unit, volume size, and volume unit
+  #   ngredient cost, cost size, cost unit, and name
+  #   recipe_ingredient amount and unit
+  #   weight_volume_conversion weight size, weight unit, volume size, and volume unit
   # Calculate: ingredient cost in this recipe
 
   def initialize(recipe_ingredient)
@@ -20,13 +20,13 @@ class CombinedIngredient
     # Look for ingredient in user_ingredient_costs table
     user_details = @user.UserIngredientCost.find_by(id: @ingredient.id)
 
-    if details
+    if user_details
       # If ingredient in user_ingredient_costs table, grab cost, cost size, and cost unit
       @cost = user_details.cost
       @cost_size = user_details.cost_size
       @cost_unit = user_details.cost_unit
     else
-      # Else grab cost, cost size, and cost unit from ingredients table
+      # Else grab cost, cost size, and cost unit from main ingredients table
       @cost = @ingredient.cost
       @cost_size = @ingredient.cost_size
       @cost_unit = @ingredient.cost_unit
@@ -34,11 +34,35 @@ class CombinedIngredient
 
     # Find total ingredient cost by amount and weight_volume_conversion table
     # If @unit is not the same as @cost_unit
-      # if @ingredient.id in weight_volume_conversions table, grab conversion data and convert
+    if @unit != @cost_unit
+
+      # Find @ingredient.id in weight_volume_conversions table
+      conversion_data = WeightVolumeConversion.find_by(id: @ingredient.id)
+
+      # If entry in weight_volume_conversions table, grab conversion data and convert
+      if conversion_data
+        # t.float "weight_size"
+        # t.string "weight_unit"
+
+        # t.float "vol_size"
+        # t.string "vol_unit"
+
+        # @cost = user_details.cost
+        # @cost_size = user_details.cost_size
+        # @cost_unit = user_details.cost_unit
+
       # else use constants to convert
-    # Calculate total cost = normalized ingredient_amount * @cost
+      else 
+
+      end
+    end 
+    
+    # Calculate total cost
     # 50lb KA flour for $50
     # 1.5 cups of flour in brownies 
+    
+    # @total_cost = @cost * converted_amount
+
 
   end
 
