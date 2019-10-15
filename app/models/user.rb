ec730e0cd1a4c8b0e809eb57b47baba6cc909c9d
aparkening <aaron.parkening@gmail.com>
2019-10-15 14:41:32 -0400

    Add password validations to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,10 @@ class User < ApplicationRecord
   has_many :flags
   has_many :user_ingredient_costs
   has_many :ingredients, through: :user_ingredient_costs
-
-  # has_many :recipe_ingredients
-  # has_many :ingredients, through: :recipe_ingredients
   
   # Validations
   validates :name, presence: true, uniqueness: true
   validates :password_digest, presence: true, on: :create
-  validates :password_digest, length: { minimum: 6 }
-  # validates :password_digest, length: { minimum: 6 }, confirmation: true, unless: ->(u){ u.password.blank? }
-  # validates :password_confirmation, presence: true, on: :create
-
+  validates :password_digest, length: { minimum: 6 }, confirmation: true, unless: ->(u){ u.password.blank? }
+  validates :password_confirmation, presence: true, on: :create
 end
