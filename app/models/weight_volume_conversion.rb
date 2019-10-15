8d968ef4a760ca17b9b95338b7521a32921d8392
aparkening <aaron.parkening@gmail.com>
2019-10-15 14:43:34 -0400

    Add validations to weight_volume_conversion model.

--- app/models/weight_volume_conversion.rb
+++ app/models/weight_volume_conversion.rb
@@ -1,4 +1,10 @@
 class WeightVolumeConversion < ApplicationRecord
   # Relationships
   belongs_to :ingredient, optional: true
+
+  # Validations
+  validates :weight_size, presence: true, numericality: true
+  validates :weight_unit, presence: true
+  validates :vol_size, presence: true, numericality: true
+  validates :vol_unit, presence: true
 end
