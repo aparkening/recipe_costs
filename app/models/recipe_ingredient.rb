8e2370868c774dda98f1311125bf65a47b9d0201
aparkening <aaron.parkening@gmail.com>
2019-10-15 14:38:20 -0400

    Add validations to recipe_ingredient model.

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,8 +1,10 @@
 class RecipeIngredient < ApplicationRecord
   # Relationships
   belongs_to :recipe
-  # belongs_to :ingredientable, polymorphic: true
-
   belongs_to :ingredient
   # belongs_to :user_ingredient_costs
+
+  # Validations
+  validates :ingredient_amount, presence: true, numericality: true
+  validates :ingredient_unit, presence: true
 end
