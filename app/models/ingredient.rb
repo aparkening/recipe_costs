1979536b807b12487b7a5976b0545bf8732f52d1
aparkening <aaron.parkening@gmail.com>
2019-10-15 14:36:38 -0400

    Add validations to ingredient model.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,4 +3,10 @@ class Ingredient < ApplicationRecord
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
   has_many :flags
+
+  # Validations
+  validates :name, presence: true, uniqueness: true
+  validates :cost, presence: true, numericality: true
+  validates :cost_size, presence: true, numericality: true
+  validates :cost_unit, presence: true
 end
