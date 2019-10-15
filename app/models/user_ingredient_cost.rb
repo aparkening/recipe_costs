44fd047dcb18093a2ec17ce55ef376a781710763
aparkening <aaron.parkening@gmail.com>
2019-10-15 14:40:21 -0400

    Add validations to user_ingredient_cost model.

--- app/models/user_ingredient_cost.rb
+++ app/models/user_ingredient_cost.rb
@@ -1,4 +1,10 @@
 class UserIngredientCost < ApplicationRecord
+  # Relationships
   belongs_to :user
   belongs_to :ingredient
+
+  # Validations
+  validates :cost, presence: true, numericality: true
+  validates :cost_size, presence: true, numericality: true
+  validates :cost_unit, presence: true
 end
