2fe378be281262b43899cd4c2d692cca88b084df
aparkening <aaron.parkening@gmail.com>
2019-10-16 12:24:55 -0400

    Add bootstrap requirements to application.js. Add custom global.js for future customizations.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,11 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
+require("jquery3").start()
+require("popper").start()
+require("bootstrap").start()
 require("channels")
-
+require("custom/global")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
