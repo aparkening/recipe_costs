c851cdc819867d22ee45713e2d17bf7f4b02ce91
aparkening <aaron.parkening@gmail.com>
2019-10-16 14:07:49 -0400

    Sort ingredients by name order.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,6 +1,6 @@
 class IngredientsController < ApplicationController
   def index
-    @ingredients = Ingredient.all
+    @ingredients = Ingredient.all.order(name: :asc)
   end
 
   # Ingredient import page
