aa55084bfda375dfe64d5c3ac74025180233aa3b
aparkening <aaron.parkening@gmail.com>
2019-10-16 11:52:14 -0400

    Add UsersController with tests, stylesheet, helpers, and view folder. Add import path to controller.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,26 +3,84 @@
 
 
 ### Console Testing
-user1 = User.last
+# user1 = User.last
 
-user1_recipe = user1.recipes.last
-ing1 = Ingredient.last
+# user1_recipe = user1.recipes.last
+# ing1 = Ingredient.last
 
-recipe1_ing1 = user1_recipe.recipe_ingredients.last
+# recipe1_ing1 = user1_recipe.recipe_ingredients.last
 
-user1_ing1 = user1.user_ingredient_costs.last
+# user1_ing1 = user1.user_ingredient_costs.last
 
-combo_ing1 = CombinedIngredient.new(recipe1_ing1)
+# combo_ing1 = CombinedIngredient.new(recipe1_ing1)
 
 
 ### Build seeds
 
-# Make User, recipe, ingredient
-user1 = User.create(name:"Steve Tester", password:"testing", role:"User", organization:"A&E Bakery")
-user1_r1 = user1.recipes.create(name:"Apple Pie", servings:8) 
+# Users
+admin = User.create(name: "David Baker", password: "testing123", password_confirmation: "testing123", role: "admin", organization: "The Culinary Institute of America")
+
+paul = User.create(name: "Paul Hollywood", password: "testing123", password_confirmation: "testing123", organization: "Great British Bake Off")
+
+prue = User.create(name: "Prue Leith", password: "testing123", password_confirmation: "testing123", organization: "Great British Bake Off")
+
+mary = User.create(name: "Mary Berry", password: "testing123", password_confirmation: "testing123", organization: "Great British Bake Off")
+
+morimoto = User.create(name: "Masaharu Morimoto", password: "testing123", password_confirmation: "testing123", organization: "Morimoto's")
+
+jamie = User.create(name: "Jamie Oliver", password: "testing123", password_confirmation: "testing123")
+
+nigel = User.create(name: "Nigel Slater", password: "testing123", password_confirmation: "testing123")
+
+harold = User.create(name: "Harold McGee", password: "testing123", password_confirmation: "testing123")
+
+rosie = User.create(name: "Judy Rosenberg", password: "testing123", password_confirmation: "testing123", organization: "Rosie's Bakery")
+
+peter = User.create(name: "Peter Reinhart", password: "testing123", password_confirmation: "testing123", organization: "Johnson and Wales University")
+
+
+# Recipes
+white_bread = paul.recipes.create(name:"White Bread", servings:8)
+english_muffins = paul.recipes.create(name:"English Muffins", servings:60)
+
+choc_torte = prue.recipes.create(name:"Flourless Chocolate Torte", servings:64) 
+
+puff_pastry = mary.recipes.create(name:"Puff Pastry", servings:92)
+ginger_cake = mary.recipes.create(name:"Ginger Cake", servings:18)
+ 
+chicken_teriyaki = morimoto.recipes.create(name:"Chicken Teriyaki", servings:4)
+onigiri = morimoto.recipes.create(name:"Chicken Teriyaki Onigiri", servings:4)
+tamagoyaki = morimoto.recipes.create(name:"Tamagoyaki", servings:4)
+
+apple_cake = jamie.recipes.create(name:"Apple Cake", servings:24)  
+
+apple_pie = nigel.recipes.create(name:"Apple Pie", servings:8) 
+pumpkin_scones = nigel.recipes.create(name:"Pumpkin Scones", servings:8) 
+
+ginger_cookies = harold.recipes.create(name:"Ginger Cookies", servings:186) 
+
+choc_chip_cookies = rosie.recipes.create(name:"Chocolate Chip Cookies", servings:240)
+macaroons = rosie.recipes.create(name:"Macaroons", servings:60)
+
+cinn_raisin_bread = peter.recipes.create(name:"Cinnamon Raisin Bread", servings:8) 
+french_bread = peter.recipes.create(name:"French Bread", servings:6)
+pizza_dough = peter.recipes.create(name:"Pizza Dough", servings:6)
+
+
+# Ingredients
+
 ing1 = Ingredient.create(name:"flour", cost:12.50, cost_size:5, cost_unit:"lb")
 
+
+
 # Add ingredient to recipe
+
+# Cinnamon Raisin Bread
+
+
+
+
+
 r1_ing1 = user1_r1.recipe_ingredients.create(ingredient:ing1, ingredient_amount:6.376, ingredient_unit:"oz")
   # get name with user1_r1.recipe_ingredients.first.ingredient.name
 
