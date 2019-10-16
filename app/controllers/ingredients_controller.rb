08dcdd6465726569e088a97451ed1a76ace066fe
aparkening <aaron.parkening@gmail.com>
2019-10-16 12:08:36 -0400

    Add import to index, controller, and routes.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,7 +1,11 @@
 class IngredientsController < ApplicationController
   def index
+    @ingredients = Ingredient.all
   end
 
+  # Ingredient import page
   def import
+    Ingredient.import(params[:file])
+    redirect_to ingredients_index, notice: "File successfully imported"
   end
 end
