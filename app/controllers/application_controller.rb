a336c0a9551cc046ba6a95be9188650d4fbd8b1d
aparkening <aaron.parkening@gmail.com>
2019-10-17 10:28:37 -0400

    Add index, current_user, and require_login logic to application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  # Display root page. If current user, show user page.
+  def index
+    redirect_to @current_user if current_user
+  end
+
+  # Set current user
+	def current_user
+	  @current_user ||= User.find(session[:user_id]) if session[:user_id].present?
+	end
+  helper_method :current_user
+  
+  private
+	 
+  def require_login
+    redirect_to root_path unless current_user
+  end
+
 end
