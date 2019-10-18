8f959dec1e63393eba670f042cf010d5685c596e
aparkening <aaron.parkening@gmail.com>
2019-10-18 11:57:10 -0400

    Add uid field to database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_18_022157) do
+ActiveRecord::Schema.define(version: 2019_10_18_153049) do
 
   create_table "categories", force: :cascade do |t|
     t.integer "user_id"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2019_10_18_022157) do
     t.string "google_token"
     t.string "google_refresh_token"
     t.string "email"
+    t.string "uid"
   end
 
   create_table "weight_volume_conversions", force: :cascade do |t|
