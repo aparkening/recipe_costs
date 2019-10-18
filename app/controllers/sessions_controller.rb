83d217456da5ec1d1e44ed2010b7aabc1a593673
aparkening <aaron.parkening@gmail.com>
2019-10-18 11:10:44 -0400

    Refactor to use explicit flash messages.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,31 +12,43 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      redirect_to login_path, error: "Credentials don't work. Please check your name and password." 
+      flash[:error] = "Credentials don't work. Please check your name and password." 
+      redirect_to login_path
     end
   end
 
   # Log user out
   def destroy
     session.clear
-    redirect_to root_path, notice: "Successfully logged out"
+    flash[:notice] = "Successfully logged out"
+    redirect_to root_path
   end
 
-  # Google Oauth
+  # Google Oauth Login
   def googleAuth
-    # Get access tokens from the google server
-    access_token = request.env["omniauth.auth"]
-    user = User.from_omniauth(access_token)
-    log_in(user)
+    # Get access tokens from the google server; find or create user
+    user = User.find_or_create_by_omniauth(auth)
+
     # Access_token is used to authenticate request made from the rails application to the google server
-    user.google_token = access_token.credentials.token
+    user.google_token = auth.credentials.token
+    
     # Refresh_token to request new access_token
     # Note: Refresh_token is only sent once during the first request
-    refresh_token = access_token.credentials.refresh_token
+    refresh_token = auth.credentials.refresh_token
     user.google_refresh_token = refresh_token if refresh_token.present?
-    user.save
-    session[:user_id] = user.id
-    redirect_to user_path(user)
+
+    if user.save
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:error] = "Credentials don't work. Please try again."
+    end
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 
 end
