ba38e7b19595ee426e62943557b15ac2f04738d4
aparkening <aaron.parkening@gmail.com>
2019-10-17 22:32:47 -0400

    Create Google user in sessions controller. Update routes to pull :provider. Update from_omniauth to search for name, rather than email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     # Creates a new user only if it doesn't exist
-    where(email: auth.info.email).first_or_initialize do |user|
+    where(name: auth.info.name).first_or_initialize do |user|
       user.name = auth.info.name
       user.email = auth.info.email
     end
