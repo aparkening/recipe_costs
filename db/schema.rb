bb87eba81150691552e63b861f19d5d8cd6ad32e
aparkening <aaron.parkening@gmail.com>
2019-10-17 22:23:34 -0400

    Add env to gitignore. Update user migration to include email for google oauth.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_18_015824) do
+ActiveRecord::Schema.define(version: 2019_10_18_022157) do
 
   create_table "categories", force: :cascade do |t|
     t.integer "user_id"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 2019_10_18_015824) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "google_token"
     t.string "google_refresh_token"
+    t.string "email"
   end
 
   create_table "weight_volume_conversions", force: :cascade do |t|
