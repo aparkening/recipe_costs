0d6d108c55320579dadbf749230e6a8f13acadfb
aparkening <aaron.parkening@gmail.com>
2019-10-17 22:00:38 -0400

    Add google token migration and oauth routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,18 @@ Rails.application.routes.draw do
   
   resources :users
 
+  # Session
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
-
+  # Ingredients
   get 'ingredients/index'
   post 'ingredients/import'
 
+  # Google authentication
+  get 'auth/:provider/callback', to: 'sessions#googleAuth'
+  get 'auth/failure', to: redirect('/')
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
