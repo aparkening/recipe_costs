631451f8f94f6f353d1bb3cd1996a8d4b54d7bcd
aparkening <aaron.parkening@gmail.com>
2019-10-20 19:29:10 -0400

    Add delete to form and controller.

--- app/controllers/user_ingredient_costs_controller.rb
+++ app/controllers/user_ingredient_costs_controller.rb
@@ -12,6 +12,9 @@ class UserIngredientCostsController < ApplicationController
 
   # Display new form
   def new
+    redirect_non_users
+
+    @user = User.find_by(id: params[:user_id])
     @user_ingredient = Ingredient.new
   end
 
@@ -19,17 +22,20 @@ class UserIngredientCostsController < ApplicationController
 def create
   redirect_non_users
   
-  user = User.find_by(id: params[:user_id])
-  ingredient = Ingredient.find(id: params[:ingredient_id])
+  @user = User.find_by(id: params[:user_id])
+  params[:ingredient][:ingredient_id] = params[:ingredient][:id]
+
+  # ingredient = Ingredient.find(params[:ingredient][:id])
+
 
   # Ensure current user can create for user
-  require_authorization(user)
+  require_authorization(@user)
 
     # Create ingredient
-    user_ingredient = UserIngredientCost.new(ing_params)
+    @user_ingredient = @user.user_ingredient_costs.build(ing_params)
   
-    if user_ingredient.save
-      redirect_to user_ingredient_cost_path(user, user_ingredient)
+    if @user_ingredient.save
+      redirect_to user_ingredients_path(@user)
     else
       # flash[:error] = ingredient.errors.full_messages
       # redirect_to new_ingredient_path
@@ -46,13 +52,26 @@ def create
   def update
   end
 
+  # Delete record
   def destroy
+    redirect_non_users
+    user = User.find_by(id: params[:user_id])
+
+    # Require authorization
+    require_authorization(user)
+
+    # Find and destroy ingredient
+    user_ingredient = user.user_ingredient_costs.find(params[:id])
+    user_ingredient.destroy
+
+    flash[:notice] = "Custom ingredient removed."
+    redirect_to user_ingredients_path(user)
   end
 
   private
 
   def ing_params
-    params.require(:user_ingredient).permit(:ingredient_id, :cost, :cost_size, :cost_unit)
+    params.require(:ingredient).permit(:id, :ingredient_id, :cost, :cost_size, :cost_unit)
   end
 
 end
