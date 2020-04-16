805fbd133544e4507bc6835d675a7e4d243767fe
aparkening <aaron.parkening@gmail.com>
2020-04-16 15:39:11 -0400

    Add validation for unique ingredient per user.

--- app/models/user_ingredient_cost.rb
+++ app/models/user_ingredient_cost.rb
@@ -4,7 +4,9 @@ class UserIngredientCost < ApplicationRecord
   belongs_to :ingredient
 
   # Validations
+  validates :ingredient_id, uniqueness: { scope: :user_id, message: "already exists. Only one custom cost allowed per ingredient." }
   validates :cost, presence: true, numericality: true
   validates :cost_size, presence: true, numericality: true
   validates :cost_unit, presence: true
+
 end
