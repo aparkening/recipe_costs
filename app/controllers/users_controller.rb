2a5a8de0827805b6a3259527af3cdae227b9cd7c
aparkening <aaron.parkening@gmail.com>
2020-04-20 17:50:48 -0400

    Consolidate @user creation into set_user before_action.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   before_action :require_login
+  before_action :set_user
   skip_before_action :require_login, only: [:new, :create]
+  skip_before_action :set_user, only: [:index, :new, :create]
 
-  # Display all users
+  # Display all users if admin
   def index
     if is_admin?
       @users = User.all.order(name: :asc)
@@ -13,94 +15,96 @@ class UsersController < ApplicationController
     end
   end
 
-  # Display user page
-  def show
-    redirect_non_users
-    @user = User.find(params[:id])
-    require_authorization(@user)
-
-    # Get all ingredients used by user
-    @ingredients = @user.recipes.all_ingredients
-  end
-
-  # Display signup form
+  # Display new form
   def new
     @user = User.new
   end
   
-  # Create user
+  # Create record
   def create
     # Set correct admin data if box checked
     params[:user][:admin] = "true" if params[:user][:admin] && params[:user][:admin] == "1"
 
+    # Create user
     @user = User.new(user_params)
+
+    # Set session and redirect unless admin or error
     if @user.save
-      # Set session if current user isn't an admin
       if is_admin?
-        redirect_to users_path, notice: "#{@user.name} successfully created."
+        flash[:success] = "Success! #{@user.name} created."
+        redirect_to users_path
       else 
         session[:user_id] = @user.id
         redirect_to user_path(@user)
       end
     else
-      # flash[:error] = @user.errors.full_messages
-      # redirect_to new_user_path
       render :new
     end
   end
 
+  # Display record
+  def show
+    redirect_non_users
+    require_authorization(@user)
+
+    # Get all ingredients used by user
+    @ingredients = @user.recipes.all_ingredients
+  end  
+
   # Display edit form
   def edit
     redirect_non_users
-    @user = User.find(params[:id])
 
-    # Require authorization
     require_authorization(@user)
   end
  
-  # Update user
+  # Update record
   def update
     redirect_non_users
-    @user = User.find(params[:id])
-
-    # Require authorization
     require_authorization(@user)
 
     # Set correct admin data if box checked
     params[:user][:admin] = "true" if params[:user][:admin] && params[:user][:admin] == "1"
 
+    # Update record
     @user.update(user_params)
 
+    # Redirect unless error
     if @user.save
-      # Set session if current user isn't an admin
       if @user == current_user
         redirect_to @user, notice: "Success! You're updated."
       else 
         redirect_to users_path, notice: "#{@user.name} successfully updated."
       end
     else
-      # flash[:error] = @user.errors.full_messages
-      # flash[:error] = "Credentials don't work. Please ensure your passwords match."
       render :edit
     end
   end
 
-  # Delete user
+  # Delete record
   def destroy
-    # Ensure only admins can delete users
+    # Only admins can delete users
     if is_admin?
-      @user = User.find(params[:id])
-      @user.destroy
-      flash[:notice] = "User deleted"
+      # Destroy unless error
+      if @user
+        flash[:success] = "Success! #{@user.name.titleize} deleted."
+        user.destroy
+      else
+        flash[:alert] = "Custom cost not found."
+      end
+
       redirect_to users_path
     else
       redirect_to root_path, error: "You're not authorized to change this resource."
     end
   end
 
-
   private
  
+  def set_user
+    @user = User.find_by(id: params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:name, :password, :password_confirmation, :organization, :email, :admin)
   end
