dd2b6cfe114ed289b6af38c72fbedfe922402acb
aparkening <aaron.parkening@gmail.com>
2020-04-28 16:59:49 -0400

    Remove jquery and popper from requires and remove .start() from all requires to fix Webpacker compile error.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("jquery3").start()
-require("popper").start()
-require("bootstrap").start()
-import "channels"
-import "custom/global"
+require("@rails/ujs");
+require("turbolinks");
+require("@rails/activestorage");
+require("bootstrap");
+require("channels");
+require("custom/global");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
